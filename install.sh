ed14ecaf2cbe5e6de6a9a08b77cfe7e951bd8b98
chsnv <coshgun.hasanov@gmail.com>
2023-11-16 00:12:24 +0400

    Rootless Docker and Compose installation

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt remove docker docker.io containerd runc
+
+#Adding Docker’s official GPG key:
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+
+# Setting repository:
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update your Ubuntu as well:
+sudo apt update -y
+sudo apt-cache policy docker-ce
+
+# Install docker with compose plugin:
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Setting Rootless mode:
+sudo usermod -aG docker $(whoami)
+
+# If need to check status and version of docker
+sudo systemctl status docker 
+docker -v
